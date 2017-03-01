d931fe0b1f3e6c1b85d602ff239cff6180335327
Ben <prmera@gmail.com>
2017-03-01 19:55:59 +1300

    fix a bug where author's names ending in 's' were not fucked

--- chrome_extension/content.js
+++ chrome_extension/content.js
@@ -1,6 +1,6 @@
 var author = document.getElementsByClassName("authorName")[0].innerHTML;
 
-var regex = /([A-Za-z ]+[a-z])\s+'s Opinion/;
+var regex = /([A-Za-z ]+[a-z])\s+'s? Opinion/;
 
 var newauthor = author.replace(regex, "Fuck $1's Opinion");
 

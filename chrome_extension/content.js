e34b0af13231b117e748b33e87f3aa8a994f99d4
Ben <prmera@gmail.com>
2017-03-02 06:51:54 +1300

    tidy and add comment

--- chrome_extension/content.js
+++ chrome_extension/content.js
@@ -1,9 +1,11 @@
+
+// fix whitespace between author's name and apostrophe
+// add "Fuck [author's] opinion"
+
 var author = document.getElementsByClassName("authorName")[0].innerHTML;
-
 var regex = /([A-Za-z ]+[a-z])\s+'(s?) Opinion/;
-
 var newauthor = author.replace(regex, "Fuck $1'$2 Opinion");
-
 document.getElementsByClassName("authorName")[0].innerHTML = newauthor;
+
 
 
